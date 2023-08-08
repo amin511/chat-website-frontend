50e06fab636c3eae65ba88288d4bfeec1409e85d
amin511 <amindarmouni3@gmail.com>
2023-08-08 19:27:25 +0100

    first version

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import SignUp from './component/Sign/SignUp'
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Room from './component/chatRoom/Room'
-import Users from './component/users/users'
+import Users from './component/users/Users'
 import { useSelector } from 'react-redux'
 import Home from './component/Home'
 
