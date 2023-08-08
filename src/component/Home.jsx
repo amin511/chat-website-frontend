ba57d11afcac64ec69b49133809ff071dd45ed46
amin511 <amindarmouni3@gmail.com>
2023-08-08 20:02:52 +0100

    update route

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
             <h1 className='text-4xl max-w-max mx-auto mt-10'>Home </h1>
             {user ?
                 <div className='space-y-3'>
-                    <h2 className='text-2xl mx-auto max-w-max mt-10 font-mono'>{`welcome ${user.name}`}</h2>
+                    <h2 className='text-2xl mx-auto max-w-max mt-10 font-mono'>{`Welcome  ${user.name} !`}</h2>
                     <Link to={"/users"}><div className='bg-blue-400 max-w-max p-4 px-8 rounded-md text-2xl font-serif mx-auto mt-10'>Chat app</div></Link>
                     <Logout />
                 </div>
