f1675f6e5046a640213405cfeb22df8966f49d13
amin511 <amindarmouni3@gmail.com>
2023-09-20 00:44:22 +0100

    some changes : track users online with socket.io , hanle the images using  multer

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Logo from "../../public/images/logo.svg"
+import { LogoIcon } from '../IconsSvg'
 const Header = ({ title }) => {
     return (
         <header className='flex flex-col items-center justify-center gap-5'>
-            <img className='w-40 h-10 ' src={Logo} />
-            <h1 className='text-[30px] font-Georgia font-bold text-[24px] text-primary-700 max-w-max mx-auto '>
+            <LogoIcon className={"w-40 h-10"} />
+            <h1 className=' font-Georgia font-bold text-[24px] text-primary-700 max-w-max mx-auto '>
                 {title}
             </h1>
         </header>
